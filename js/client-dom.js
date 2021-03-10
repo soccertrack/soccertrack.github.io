bfc16200919225664abbd6a68e9da967001dfb64
Kenny Lim <kennylim@microsoft.com>
2021-03-10 10:50:28 -0800

    First version

---
+++ js/client-dom.js
@@ -0,0 +1,45 @@
+
+function processFilterBlock() {
+    
+    const filterElement = document.getElementById('filterBlock');
+    const blockState = (filterElement.style.display == 'none');   
+
+    if (blockState) {
+        filterElement.style.display = 'inline';
+    } else {
+        filterElement.style.display = 'none';
+    }
+}
+
+function filterByLeague(country) {
+    const container = document.getElementById('cardContainer');
+    const children = container.children;
+
+    for(var i=0; i<children.length; ++i) {
+        
+        const countryData = children.item(i).getAttribute('data-league').toLowerCase();
+
+        if(countryData == country || country == "all") {
+            children.item(i).style.display = 'inline';
+        } else {
+            children.item(i).style.display = 'none';
+        }
+    }
+}
+
+function filterByPosition(position) {
+    const container = document.getElementById('cardContainer');
+    const children = container.children;
+
+    position = position.toLowerCase();
+    for(var i=0; i<children.length; ++i) {
+        
+        const positionData = children.item(i).getAttribute('data-position').toLowerCase();
+
+        if(positionData == position || position == "all") {
+            children.item(i).style.display = 'inline';
+        } else {
+            children.item(i).style.display = 'none';
+        }
+    }
+}
